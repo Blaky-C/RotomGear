8bce3e548546349f8fe4e87a3b12e2f39e9ed049
Jack <ch983702292@gmail.com>
2017-09-07 01:21:42 +0800

    完成初步界面设计，完善身份证号码解析功能

--- app/src/main/java/com/example/rotomugear/IDCheckActivity.java
+++ app/src/main/java/com/example/rotomugear/IDCheckActivity.java
@@ -1,8 +1,12 @@
 package com.example.rotomugear;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,9 +15,28 @@ import com.example.rotomugear.logic.IDCheck;
 
 public class IDCheckActivity extends AppCompatActivity {
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_son, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private EditText inputText;
     private FloatingActionButton fab;
     private TextView outputText;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +46,13 @@ public class IDCheckActivity extends AppCompatActivity {
         inputText = (EditText)findViewById(R.id.text_input);
         fab = (FloatingActionButton)findViewById(R.id.resolve);
         outputText = (TextView)findViewById(R.id.text_resolve_result);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        actionBar.setTitle(getResources().getString(R.string.id_check).toString());
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/rotomugear/MainActivity.java
+++ app/src/main/java/com/example/rotomugear/MainActivity.java
@@ -1,16 +1,77 @@
 package com.example.rotomugear;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_setup:
+                Toast.makeText(this, "Function hasn't been finished.", Toast.LENGTH_SHORT).show();
+                return true;
+            case android.R.id.home:
+                drawerLayout.openDrawer(drawer);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private DrawerLayout drawerLayout;
+    private NavigationView drawer;
+    private Button exitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.icon_menu);
+        }
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawer = (NavigationView)findViewById(R.id.drawer);
+        exitButton = (Button)findViewById(R.id.exit);
+        drawer.setCheckedItem(R.id.mine);
+        drawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawer(drawer);
+                return true;
+            }
+        });
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawer(drawer);
+                finish();
+            }
+        });
     }
 
     public void IDCheckActivityStart(View view){
